4cd1c16d2cfecc751c88f152cac91d6762804957
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2025-01-22 16:58:10 +0530

    deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import ApplyForceExample from "@/components/ApplyForce";
-import PhysicsCanvas from "@/components/Canvas";
-import CreateGifWithGifJs from "@/components/CCapture";
-import CreateGifExample from "@/components/CCapture";
-import CompositeExample from "@/components/Composite";
-import CompositesExample from "@/components/Composites";
-import BodyConstrains from "@/components/Constrains";
+// import ApplyForceExample from "@/components/ApplyForce";
+// import PhysicsCanvas from "@/components/Canvas";
+// import CreateGifWithGifJs from "@/components/CCapture";
+// import CreateGifExample from "@/components/CCapture";
+// import CompositeExample from "@/components/Composite";
+// import CompositesExample from "@/components/Composites";
+// import BodyConstrains from "@/components/Constrains";
 import DetailedObjectDrawing from "@/components/DatailedDraw";
-import PhysicsEngine from "@/components/Engine";
-import PauseAndFastForward from "@/components/PauseFast";
-import PauseRenderingExample from "@/components/PauseRender";
-import DrawAndConvert from "@/components/vertices";
+// import PhysicsEngine from "@/components/Engine";
+// import PauseAndFastForward from "@/components/PauseFast";
+// import PauseRenderingExample from "@/components/PauseRender";
+// import DrawAndConvert from "@/components/vertices";
 
 export default function Home() {
   return (
